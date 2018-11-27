ae334ec5dabb7779b842a382372b66ebd8004252
Abdurrehman Siddiqui <abdurrehman135@gmail.com>
2018-11-26 20:49:23 -0500

    HTML and CSS properly handled by Flask.

--- flaskwebsite.py
+++ flaskwebsite.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html')
 
 @app.route("/install")
 def about():
-    return "<h1>Installation of frameworks tutorial</h1>"
+    return render_template('install.html', title='Installing Frameworks')
 
 if __name__ == "__main__":
     app.run(debug=True)
